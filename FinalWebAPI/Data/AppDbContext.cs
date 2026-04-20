59e88a15e2f1d5d5fb4178e41826021bee196532
Maria Smith <maria.t.smith023@gmail.com>
2026-04-20 15:58:25 -0400

    dbContext, create table, first migration

---
+++ FinalWebAPI/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using FinalWebAPI.Models;
+
+namespace FinalWebAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<TeamMember> TeamMembers { get; set; }
+    }
+}
--- FinalWebAPI/Program.cs
+++ FinalWebAPI/Program.cs
@@ -1,25 +1,25 @@
+using FinalWebAPI.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using NSwag.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddOpenApiDocument();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseOpenApi();        // serves /swagger/v1/swagger.json
+app.UseSwaggerUi();      // serves the Swagger UI page
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
